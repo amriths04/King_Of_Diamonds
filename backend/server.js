a86dc087c7cf85c15689e28938c1d2069bb722fa
amriths04 <89180140+amriths04@users.noreply.github.com>
2025-08-22 19:34:45 +0530

    hosting backned

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,24 @@ import express from "express";
 import cors from "cors";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import dotenv from "dotenv";
 import roomRoutes from "./routes/roomRoutes.js";
 import initSocket from "./socket.js";
+dotenv.config();
 
 const app = express();
 const httpServer = createServer(app);
+const FRONTEND_URL = process.env.FRONTEND_URL_LOCAL;
 
 const io = new Server(httpServer, {
-  cors: { origin: "http://localhost:5173", methods: ["GET","POST","PUT","DELETE"], credentials: true }
+  cors: { origin: FRONTEND_URL, methods: ["GET","POST","PUT","DELETE"], credentials: true }
 });
 
-app.use(cors({ origin: "http://localhost:5173", methods: ["GET","POST","PUT","DELETE"], allowedHeaders: ["Content-Type"] }));
+app.use(cors({ origin: FRONTEND_URL, methods: ["GET","POST","PUT","DELETE"], allowedHeaders: ["Content-Type"] }));
 app.use(express.json());
 app.use("/api/rooms", roomRoutes);
 
 initSocket(io);
 
-httpServer.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
